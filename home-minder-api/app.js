d736bf6bbe40f84aa0f0ade075cca680486fc37a
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-04-13 14:05:35 -0600

    added put by id to backend

--- home-minder-api/app.js
+++ home-minder-api/app.js
@@ -12,9 +12,31 @@ app.get('/api/v1/home', (request, response) => {
 })
 
 app.get('/api/v1/:id', (request, response) => {
-  const itemById = app.locals.filter(item => request.id == item.id)
-  console.log(itemById)
+  const itemById = app.locals.home.filter(item => request.id == item.id)
   return response.status(200).json(itemById)
+})
+
+app.put('/api/v1/:id', (request, response) => {
+  const { home } = app.locals.home
+  
+  let found = false
+  const updatedHomeData = home.map(item => {
+    if (request.id == item.id) {
+      found = true
+      return {
+        brand: request.brand,
+        model: request.model,
+        purchaseLocation: request.purchaseLocation,
+        userScheduled: request.userScheduled,
+      }
+    } else {
+      return item
+    }
+  })
+  
+  home = updatedHomeData
+  console.log(home)
+  return response.status(204)
 })
 
 export default app;
