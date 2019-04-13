aae920d7a8c3b627f0e7fc5f1f2adc5d36baab26
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-04-13 14:05:57 -0600

    add put item thunk

---
+++ home-minder/src/thunks/putUpdatedItem.js
@@ -0,0 +1,18 @@
+import { isLoading, hasError } from "../actions";
+
+export const putUpdatedItem = (url, options) => {
+  return async (dispatch) => {
+    try {
+      dispatch(isLoading(true))
+      const response = await fetch(url, options)
+      if(!response.ok) {
+        throw Error(response.statusText)
+      }
+      dispatch(isLoading(false))
+      const result = await response.json()
+      dispatch(updateItem(result))
+    } catch (error) {
+      dispatch(hasError(error.message))
+    }
+  }
+}
